069c060387101243d1283eaddcc091a366a0114c
hari <harianandmca@yahoo.co.in>
2020-08-18 17:57:52 +0530

    commited by indian user

--- JavExamp/src/io/hari/FactoryDeisgnPatternImp/DemoTest.java
+++ JavExamp/src/io/hari/FactoryDeisgnPatternImp/DemoTest.java
@@ -4,4 +4,12 @@ public class DemoTest {
 	public DemoTest(){
 		System.out.println("This is Demo Class");
 	}
+	
+	public void DemoTest1(String one){
+		System.out.println("This is Demo Class");
+	}
+	
+	public void DemoTest2(int i){
+		System.out.println("This is Demo Class");
+	}
 }
